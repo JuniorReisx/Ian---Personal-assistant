ca6cc13fd99dcda1beec9554e4ecf99e4b9050d5
JuniorReisx <gilmaikjunior2@gmail.com>
2025-11-22 13:36:34 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ← IMPORTANTE: deve ser '/'
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true
   }
 })
+
