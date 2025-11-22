30e53ba32c8949a02c36063d831711de559cc6d9
JuniorReisx <gilmaikjunior2@gmail.com>
2025-11-22 12:44:59 -0300

    update vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante para paths relativos
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
     }
   }
